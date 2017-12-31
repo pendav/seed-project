2160d434d6baeb8ffc7ed7f6b30cd9e2409a34f2
David Peña Aguilar <David@MacBook-Pro-de-David.local>
2017-12-31 11:56:13 -0600

    Add advance of the course

---
+++ assets/app/auth/user.model.ts
@@ -0,0 +1,6 @@
+export class User {
+	constructor(public email: String, 
+				public password: string, 
+				public firstName?: string, 
+				public lastName?: string){}
+}
---
+++ assets/app/messages/message-input.component.ts
@@ -0,0 +1,17 @@
+import { Component } from "@angular/core";
+
+import { MessageService } from "./message.service";
+import { Message } from "./message.model";
+
+@Component({
+	selector: 'app-message-input',
+	templateUrl: './message-input.component.html'
+})
+export class MessageInputComponent{
+	constructor(private messageService: MessageService){}
+
+	onSave(value: String){
+		const message = new Message(value, 'David Peña')
+		this.messageService.addMessage(message);
+	}
+}
